fcd82979a5921fe0ce1a8509b3dcf84ee042b504
“Christine <csalonen89@gmail.com>
2021-06-18 14:53:20 -0700

    make Swift result have each value equal to 1. Make Rust result equal to or greater then eight. Make Python result equal to or less then seven to help produce accurate answers on form.

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,19 @@ $(document).ready(function() {
     const val4 = parseInt($("#breakfast").val());
     const val5 = parseInt($("#animal").val());
 
-    if (val1 + val2 + val3 + val4 + val5 <= 5) {
+    if (val1 === 0 || val2 === 0 || val3 === 0 || val4 === 0 || val5 === 0) {
       language = "try again";
-    }
-    else if (val1 + val2 + val3 + val4 + val5 >= 10) {
-      language = "Python";
-    }
-    else if (val1 + val2 + val3 + val4 + val5 >= 15) {
+    } 
+    else if (val1 === 1 && val2 === 1 && val3 === 1 && val4 === 1 && val5 === 1) {
+      language = "Swift";
+      }
+    else if (val1 + val2 + val3 + val4 + val5 >= 8) {
       language = "Rust";
     }
-    else if (val1 + val2 + val3 + val4 + val5 >= 16) {
-    language = "Swift";
+    else if (val1 + val2 + val3+ val4+ val5 <= 7) {
+      language = "Python";
     }
     else {
-      language = "try again";
     }
 
   $("#results").show();
