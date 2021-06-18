4b9a068bd962395af5abca1e88990e9e1fa945f7
“Christine <csalonen89@gmail.com>
2021-06-18 10:20:00 -0700

    add branching to create three results for Python, Rust, and Swift for users.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
   $("#lang-form").submit(function(event) {
     event.preventDefault();
+    const val1 = parseInt($("#fav-color").val());
+    const val2 = parseInt($("#location").val());
+    const val3 = parseInt($("#rain").val());
+    const val4 = parseInt($("#breakfast").val());
+    const val5 = parseInt($("#animal").val());
 
+    if (val1 + val2 + val3 + val4 + val5 <= 5) {
+      language = "try again";
+    }
+    else if (val1 + val2 + val3 + val4 + val5 >= 10) {
+      language = "Python";
+    }
+    else if (val1 + val2 + val3 + val4 + val5 >= 15) {
+      language = "Rust";
+    }
+    else if (val1 + val2 + val3 + val4 + val5 >= 16) {
+    language = "Swift";
+    }
+    else {
+      language = "try again";
+    }
 
   $("#results").show();
-  $("#language-result").text(language);
+  $("#language").text(language);
   });
-
 });
